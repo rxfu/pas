de77176c139f411cbe881d9b93b89a5cbbabc526
Stephen <stephen.frx@gmail.com>
2018-01-13 21:34:13 +0800

    update score department

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -215,7 +215,6 @@ class ScoreController extends Controller {
 	public function getDepartment($id) {
 		$department = Department::findOrFail($id)->name;
 		$indices    = Index::with('subindices')->orderBy('order')->get();
-		$scores     = Score::whereDepartmentId($id)->get();
 
 		$items = [];
 		foreach ($indices as $index) {
@@ -234,10 +233,10 @@ class ScoreController extends Controller {
 							$val = $scores->avg('score');
 						} else {
 							$scores = DB::table('scores')
-								->select(DB::raw('marker_id, department_id, index_id, SUM(score) AS score'))
-								->groupBy('marker_id', 'department_id', 'index_id')
+								->select(DB::raw('marker_id, index_id, SUM(score) AS score'))
+								->groupBy('marker_id', 'index_id')
 								->where('year', '=', date('Y'))
-								->where('department_id', '=', $department->id)
+								->whereDepartmentId($id)
 								->whereIndexId($index->id)
 								->get();
 
@@ -246,8 +245,6 @@ class ScoreController extends Controller {
 							} else {
 								$val = $scores->avg('score');
 							}
-
-							break;
 						}
 					}
 
@@ -270,23 +267,33 @@ class ScoreController extends Controller {
 					];
 				}
 			} else {
-				if (in_array(session('department'), explode(',', $index->departments))) {
-					$isCollege = Department::find(session('department'))->is_college;
+				$scores = Score::whereDepartmentId($id)
+					->whereIndexId($index->id)
+					->where('year', '=', date('Y'))
+					->get();
 
-					if (($index->id == 8) && $isCollege && (!session('is_manager'))) {
-						continue;
+				if (count($scores)) {
+					if (count(explode(',', $index->departments)) < 15) {
+						$val = $scores->avg('score');
+					} else {
+						if (2 < $scores->count()) {
+							$val = ($scores->sum('score') - $scores->min('score') - $scores->max('score')) / ($scores->count() - 2);
+						} else {
+							$val = $scores->avg('score');
+						}
 					}
-
-					$items[$index->id] = [
-						'seq'         => $index->seq,
-						'name'        => $index->name,
-						'score'       => $index->score,
-						'description' => $index->description,
-					];
 				}
+
+				$items[$index->id] = [
+					'seq'         => $index->seq,
+					'name'        => $index->name,
+					'score'       => $index->score,
+					'description' => $index->description,
+					'value'       => $val,
+				];
 			}
 		}
 
-		return view('score.indices', compact('department', 'items'));
+		return view('score.department', compact('department', 'items'));
 	}
 }
